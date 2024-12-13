c83984d58d2d21937f635c34a7670b4940660877
abdessamad <elmaarpro@gmail.com>
2024-12-13 13:25:11 +0100

    solve problem when disconnect from google and set toggle function to refresh page in logingoogle

--- portfolio/src/components/parts/NavBar/LoginGoogle.jsx
+++ portfolio/src/components/parts/NavBar/LoginGoogle.jsx
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth, provider, signInWithPopup, signOut } from "../../../firebase";
 import { UseStateContext } from "../../../context/ContextProvider";
 import AddTechnologyButton from "../AddTechnologyButton";
 export default function LoginGoogle(){
     const {user,token,showLogin ,setUser}=UseStateContext();
+    const [mount,setMount]=useState(true);
+
     const handleLogin = async () => {
         try {
           const result = await signInWithPopup(auth, provider);
-          if( Object.keys(result.user).length>0){
-            setUser(result.user); // L'utilisateur connecté
-
-            localStorage.setItem("usergoogle",result.user);
-
-          }else{
-            setUser(null)
+          if(result.user && typeof result.user==="object"){
+            if( Object.keys(result.user).length>0){
+                setUser(result.user); // L'utilisateur connecté
+    
+                localStorage.setItem("email",result.user.email);
+                localStorage.setItem("displayName",result.user.displayName
+                );
+                localStorage.setItem("photoURL",result.user.photoURL
+                );
+               
+              }
           }
+         
         } catch (error) {
           console.error("Erreur lors de la connexion :", error);
         }
       };
+      const toggleComponent = () => {
+        setMount(false); // Unmount
+        setTimeout(() => setMount(true), 0); // Remount
+      };
       const handleLogout = async () => {
         try {
           await signOut(auth);
-          setUser(null);
+        //   setUser(null);
+          localStorage.clear()
+          toggleComponent();
         } catch (error) {
           console.error("Erreur lors de la déconnexion :", error);
         }
@@ -31,11 +44,11 @@ export default function LoginGoogle(){
 
              <div className="authgoogle">
                                 <div>
-                                    {console.log(user)}
-                                    {!(Object.keys(user).length===0) ? (
+                                    {console.log( user?.length )}
+                                    {localStorage.getItem("displayName") ? (
                                         <div className="flex items-center">
-                                        <h1>Bienvenue, {user.displayName}</h1>
-                                        <img className="rounded-full w-12 mx-2" src={user.photoURL} alt="Avatar" />
+                                        <h1>Bienvenue, {localStorage.getItem("displayName")}</h1>
+                                        <img className="rounded-full w-12 mx-2" src={localStorage.getItem("photoURL")} alt="Avatar" />
                                         <button onClick={handleLogout}>Logout</button>
                                         <AddTechnologyButton/>
                                         </div>
